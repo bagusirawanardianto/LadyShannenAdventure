ba1b400070fa2a9f5915aa3921dfd1bdc907e8fb
JulianShaden <shadenjulian@gmail.com>
2019-11-23 15:02:02 +0700

    Bisa sampe Finish

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class FollowTarget : MonoBehaviour
 {
     public Transform player;
-    public Transform Bg1;
-    public Transform Bg2;
-    public Transform Bg3;
+    //public Transform Bg1;
+    //public Transform Bg2;
+    //public Transform Bg3;
+    //public Transform Bg4;
 
     // Use this for initialization
     void Start()
@@ -18,13 +19,14 @@ public class FollowTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player.position.x != transform.position.x && player.position.x > 0 && player.position.x < 12f)
+        if (player.position.x != transform.position.x && player.position.x > 0 && player.position.x < 60f)
         {
             transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, transform.position.y, transform.position.z), 0.1f);
         }
-        Bg1.transform.position = new Vector2(transform.position.x * 1.0f, Bg1.transform.position.y);
-        Bg2.transform.position = new Vector2(transform.position.x * 0.8f, Bg2.transform.position.y);
-        Bg3.transform.position = new Vector2(transform.position.x * 0.6f, Bg3.transform.position.y);
+        //Bg1.transform.position = new Vector2(transform.position.x * 1.0f, Bg1.transform.position.y);
+        //Bg2.transform.position = new Vector2(transform.position.x * 0.8f, Bg2.transform.position.y);
+        //Bg3.transform.position = new Vector2(transform.position.x * 0.6f, Bg3.transform.position.y);
+        //Bg4.transform.position = new Vector2(transform.position.x * 0.4f, Bg4.transform.position.y);
 
     }
 }
