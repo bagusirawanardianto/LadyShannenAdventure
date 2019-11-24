7c8982c3d4104089ae8f1ccf146e96f5e4e1144d
bagusirawanardianto <bagusirawanardianto@gmail.com>
2019-11-24 19:08:01 +0700

    Add HighScore

--- Assets/Scripts/LevelComplete.cs
+++ Assets/Scripts/LevelComplete.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class LevelComplete : MonoBehaviour
 {
+    public Text txHighScore;
+    int highscore;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        highscore = PlayerPrefs.GetInt("HS", 0);
+
+        if (Coins.score > highscore)
+           {
+	            highscore = Coins.score;
+	            PlayerPrefs.SetInt("HS", highscore);
+	        }
+
+        txHighScore.text = highscore.ToString();
     }
 
     // Update is called once per frame
